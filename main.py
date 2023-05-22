76a150575aa82f2b1f15634a1851c9f73a61395a
Rubyn973 <122668050+Rubyn973@users.noreply.github.com>
2023-05-22 19:44:53 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Union
+from gini_coff import*
+import asyncio
+
+from typing import Dict
+from uvicorn import Config, Server
+from fastapi import FastAPI
+
+class Item(BaseModel):
+    poor: float
+    midleclass: float
+    rich: float
+    poor_income: float
+    midleclass_income: float
+    rich_income: float
+
+app = FastAPI()
+
+@app.post("/items")
+async def create_item(item: Item):
+    calc = Calculator()
+    result = calc.ginicoff(item.poor, item.midleclass, item.rich, item.poor_income, item.midleclass_income, item.rich_income)
+    return result
+
+def start(app, host, port, loop = None):
+    if loop is None:
+        loop = asyncio.new_event_loop()
+    port = port
+    server_config = Config(app, host=host, port=port, loop=loop)
+    server = Server(config=server_config)
+    loop.create_task(server.serve())
+    loop.run_forever()
+start(app, 'localhost', 500)
